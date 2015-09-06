38d56075f932ac708f7e18d943bdcb3b93a89f12
Nitesh <nitesh@morajkar.com>
2015-09-06 13:46:21 +0530

    Created new .git-completion file

---
+++ .git-completion.bash
@@ -0,0 +1,8 @@
+if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
+  . /usr/local/git/contrib/completion/git-completion.bash
+fi
+GIT_PS1_SHOWDIRTYSTATE=true
+ 
+if [ -f /opt/local/etc/bash_completion ]; then
+    . /opt/local/etc/bash_completion
+fi
